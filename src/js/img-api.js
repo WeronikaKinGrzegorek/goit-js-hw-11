0f35f68dfcc35c94c50709237c36765649d8d5ef
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-09-06 12:13:17 +0200

    some beginings

---
+++ src/js/img-api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+const MY_API_KEY = '39280445-58c32c87f381aecf2e93abbca';
+axios.defaults.headers.common['x-api-key'] = MY_API_KEY;
+const BASE_API_URL = `https://pixabay.com/api/${MY_API_KEY}`;
+
+export function searchImages(userInput) {
+  const urlEncodedUserInput = userInput.split(' ').join('+');
+  return axios
+    .get(
+      `${BASE_API_URL}&q=${urlEncodedUserInput}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      errorInAxios(error);
+    });
+}
+
+function errorInAxios(error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+  }
+  console.log(error.config);
+}
---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import { searchImages } from './img-api';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const searchForm = document.querySelector('#search-form');
+const searchButton = document.querySelector('.search-button');
+const gallery = document.querySelector('.gallery');
+
+searchForm.addEventListener('click', e => {
+  const userInput = e.target.value;
+
+  searchImages(userInput);
+});
